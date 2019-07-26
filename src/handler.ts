b263333fcac200c47bd1d79762b27ae552504d25
khaledquadir <khaled.quadir@lendi.com.au>
2019-07-26 14:11:55 +1000

    add cloudformation createStack, updateStack event detection for Lambda

--- src/handler.ts
+++ src/handler.ts
@@ -1,16 +1,4 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
-import "source-map-support/register";
-
-export const index: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "StackJanitor works!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
+export const index = async (event, _context) => {
+  console.log(event);
+  console.log(_context);
 };
