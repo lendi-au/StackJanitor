525f6a3a892bf0b6dea499bccff19c90a3ab2ea9
khaledquadir <khaled.quadir@lendi.com.au>
2019-07-26 15:08:46 +1000

    fix naming and refactor

--- src/deleteCloudFormationStack.spec.ts
+++ src/deleteCloudFormationStack.spec.ts
@@ -1,8 +1,8 @@
 // import { APIGatewayEvent, Handler, Callback, Context } from "aws-lambda";
 import { index } from "./deleteCloudFormationStack";
 
-describe("handler should be called", () => {
-  test("response statusCode should be 200", () => {
+describe("deleteCloudFormationStack", () => {
+  test("deleteCloudFormationStack should be called", () => {
     expect(index(null, null));
   });
 });
--- src/logCloudFormationStack.spec.ts
+++ src/logCloudFormationStack.spec.ts
@@ -1,8 +1,8 @@
 // import { APIGatewayEvent, Handler, Callback, Context } from "aws-lambda";
 import { index } from "./logCloudFormationStack";
 
-describe("handler should be called", () => {
-  test("response statusCode should be 200", () => {
+describe("logCloudFormationStack", () => {
+  test("logCloudFormationStack should be called", () => {
     expect(index(null, null));
   });
 });
