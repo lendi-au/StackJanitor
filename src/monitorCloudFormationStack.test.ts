4cbbac417529113bb51ca6635ebd75e764ff355f
khaledquadir <khaled.quadir@lendi.com.au>
2019-08-05 16:38:40 +1000

    add dynamodb table

---
+++ src/monitorCloudFormationStack.test.ts
@@ -0,0 +1,8 @@
+// import { APIGatewayEvent, Handler, Callback, Context } from "aws-lambda";
+import { index } from "./monitorCloudFormationStack";
+
+describe("monitorCloudFormationStack", () => {
+  test("monitorCloudFormationStack should be called", () => {
+    expect(index(null, null));
+  });
+});
