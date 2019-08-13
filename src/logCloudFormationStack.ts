d055ba59d6a7becdc29b6b15549548e8d6962618
khaledquadir <khaled.quadir@lendi.com.au>
2019-08-13 16:44:49 +1000

    remove log line

--- src/logCloudFormationStack.ts
+++ src/logCloudFormationStack.ts
@@ -28,7 +28,6 @@ export const index = async (
   event: CloudFormationEvent,
   _context: Context
 ): Promise<StackJanitorStatus> => {
-  logger(event);
   let Status: string = StackTag.DISABLED;
 
   try {
