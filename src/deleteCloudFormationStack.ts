92df95995f8354902a825d838bac7afbfb85e59a
Matthew Turner <matty_t47@hotmail.com>
2019-08-15 22:50:56 +1000

    WIP: Clean up delete stack file

--- src/deleteCloudFormationStack.ts
+++ src/deleteCloudFormationStack.ts
@@ -1,44 +1,28 @@
 import { DynamoDBStreamEvent } from "aws-lambda";
-import { CloudFormation } from "aws-sdk";
+import { CloudFormation, DynamoDB } from "aws-sdk";
 import { logger } from "./logger";
 import { checkStackJanitorStatus, StackTag } from "./logCloudFormationStack";
-import { DeleteStackInput } from "aws-sdk/clients/cloudformation";
 
 const cloudFormation = new CloudFormation();
 
-export enum Action {
-  REMOVE = "REMOVE"
-}
-
-export const getStackNamesFromStreamEvent = (
-  event: DynamoDBStreamEvent
-): string[] =>
-  event.Records.filter(record => record.eventName === Action.REMOVE).map(
-    record => record.dynamodb.Keys.stackName.S
+export const index = async (event: DynamoDBStreamEvent): Promise<void> => {
+  const removeRecords = event.Records.filter(
+    record => record.eventName === "REMOVE"
   );
 
-export const deleteStack = async (
-  params: DeleteStackInput
-): Promise<boolean> => {
-  try {
-    await cloudFormation.deleteStack(params).promise();
-    return true;
-  } catch (e) {
-    logger.error(e);
-    return false;
-  }
-};
+  const items = removeRecords.map(i =>
+    DynamoDB.Converter.unmarshall(i.dynamodb.NewImage)
+  );
+  const stackNames = items.map(i => i.stackName);
 
-export const index = async (event: DynamoDBStreamEvent): Promise<boolean> => {
-  let deleteResult: boolean = false;
-  const StackNames = await getStackNamesFromStreamEvent(event);
-
-  for (let StackName of StackNames) {
-    const status = await checkStackJanitorStatus(StackName);
+  for (let stackName of stackNames) {
+    const status = await checkStackJanitorStatus(stackName);
     if (status === StackTag.ENABLED) {
-      deleteResult = await deleteStack({ StackName });
+      try {
+        await cloudFormation.deleteStack({ StackName: stackName }).promise();
+      } catch (e) {
+        logger.error(e);
+      }
     }
   }
-
-  return deleteResult;
 };
